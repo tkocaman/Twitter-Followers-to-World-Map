ca07e6e7b6459e930af581a0bb7ea03eb169364b
TARIK KOCAMAN <56571768+tkocaman@users.noreply.github.com>
2021-05-17 22:01:36 -0400

    Update app.py

--- source/app.py
+++ source/app.py
@@ -1,1 +1,24 @@
+import os
+import json
+from flask import Flask, render_template
 
+
+app = Flask(__name__)
+
+jason_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data", "location_data.json"))
+
+@app.route('/', methods=("POST", "GET"))
+
+def profile():
+    with open(jason_path) as fp:
+        data = json.load(fp)
+        dataWorld = data["dataWorld"]
+        dataUs = data["dataUs"]
+        
+    return render_template('index.html', 
+                        dataWorld=dataWorld, 
+                        dataUs=dataUs
+                        )
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
